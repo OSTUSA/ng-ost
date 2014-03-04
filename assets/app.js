91ef0f62079447e9e6d89635f4e877428e583f24
OST <ost@Josephs-MacBook-Pro-2.local>
2014-03-04 14:55:25 -0500

    initial commit of documentation page content

---
+++ assets/app.js
@@ -0,0 +1,34 @@
+angular.module('ngaDemoApp', ['nga', 'ui.bootstrap','plunker', 'ngTouch']);
+
+function MainCtrl($scope, $modal) {
+  $scope.showDownloadModal = function() {
+    var modalInstance = $modal.open({
+      templateUrl: 'downloadModal.html',
+      controller: 'DownloadCtrl'
+    });
+  };
+}
+
+var DownloadCtrl = function($scope, $modalInstance) {
+  $scope.options = {
+    minified: true
+  };
+
+  $scope.download = function (version) {
+    var options = $scope.options;
+
+    var downloadUrl = ['angular-ally-'];
+
+    downloadUrl.push(version);
+    if (options.minified) {
+      downloadUrl.push('.min');
+    }
+    downloadUrl.push('.js');
+
+    return downloadUrl.join('');
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss();
+  };
+}
